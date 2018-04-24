a93b38887c1bdabd17c3bb3b161fb9bcf8bbdbfa
Celso Bem dos Santos <zedblade.santos@gmail.com>
2018-04-24 22:59:41 +0100

    Update GITHUB_ACCOUNT value

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -121,7 +121,7 @@ done
 LOGGEDIN_USER=$(oc $ARG_OC_OPS whoami)
 OPENSHIFT_USER=${ARG_USERNAME:-$LOGGEDIN_USER}
 PRJ_SUFFIX=${ARG_PROJECT_SUFFIX:-`echo $OPENSHIFT_USER | sed -e 's/[-@].*//g'`}
-GITHUB_ACCOUNT=${GITHUB_ACCOUNT:-OpenShiftDemos}
+GITHUB_ACCOUNT=${GITHUB_ACCOUNT:-CelsoSantos}
 GITHUB_REF=${GITHUB_REF:-ocp-3.9-no-sonar}
 
 function deploy() {
